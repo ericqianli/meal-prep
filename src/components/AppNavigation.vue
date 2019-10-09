c2eeffc898d88b5a265e5965fab248949164abe7
Li Qian <ericqianli@Lis-MacBook-Pro.local>
2019-10-08 19:04:56 -0700

    save

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -10,9 +10,11 @@
             <v-list>
                 <template v-for="(item, index) in items">
                     <v-list-item :key="index">
-                        <v-list-item-content>{{
-                            item.title
-                        }}</v-list-item-content>
+                        <router-link :to="item.to">
+                            <v-list-item-content>
+                                {{ item.title }}
+                            </v-list-item-content>
+                        </router-link>
                     </v-list-item>
                     <v-divider :key="`divider-${index}`"></v-divider>
                 </template>
@@ -23,13 +25,16 @@
                 <v-icon>mdi-view-headline</v-icon>
             </v-btn>
             <v-spacer class="hidden-md-and-up"></v-spacer>
-            <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-            <v-btn text class="hidden-sm-and-down">Menu</v-btn>
+            <router-link to="/">
+                <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+            </router-link>
+            <v-btn text class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down"
+            <v-btn text class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join"
                 >JOIN</v-btn
             >
+            <v-btn text class="hidden-sm-and-down" to="/about">ABOUT</v-btn>
         </v-app-bar>
     </span>
 </template>
@@ -41,10 +46,21 @@ export default {
         return {
             appTitle: "Meal Prep",
             drawer: false,
-            items: [{ title: "Menu" }, { title: "Sign In" }, { title: "Join" }]
+            items: [
+                { title: "Menu", to: "/menu" },
+                { title: "Sign In", to: "/sign-in" },
+                { title: "Join", to: "/join" },
+                { title: "About", to: "/about" }
+            ]
         };
     }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+a,
+a:visited {
+    color: white;
+    text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,26 @@
 <template>
     <span>
         <home-hero></home-hero>
+        <home-details />
+        <home-plans />
     </span>
 </template>
 
-<script lang="ts">
+<script>
 import { Component, Vue } from "vue-property-decorator";
 
 import HomeHero from "@/components/HomeHero";
+import HomeDetails from "@/components/HomeDetails";
+import HomePlans from "@/components/HomePlans";
 
 // export default class Home extends Vue {}
 
 export default {
     name: "home",
     components: {
-        HomeHero
+        HomeHero,
+        HomeDetails,
+        HomePlans
     }
 };
 </script>
